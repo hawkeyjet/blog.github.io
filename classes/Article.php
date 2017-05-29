a42ddc9d5466a9735e2239113c97094250916122
Andy <hawkeyjet@gmail.com>
2017-05-29 21:27:28 +0300

    Article.php ready 99%

--- classes/Article.php
+++ classes/Article.php
@@ -8,20 +8,20 @@ class Article
 	public $summary = null;
 	public $content = null;
 
-	public function __construct( $data=array()) {
+	public function __construct($data=array()) {
 		if (isset( $data['id']))
 			$this->id = (int) $data['id'];
 		if (isset( $data['publicationDate']))
 			$this->publicationDate = (int)$data['publicationDate'];
 		if (isset($data['title']))
-			$this->title = preg_replace ( "/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['title']);
+			$this->title = preg_replace("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['title']);
 		if (isset($data['summary']))
-			$this->summary = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['summary']);
+			$this->summary = preg_replace("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['summary']);
 		if (isset( $data['content']))
 			$this->content = $data['content'];
 	}
 
-	public function storeFormValues ($params) {
+	public function storeFormValues($params) {
 		$this->__construct($params);
 
 		if (isset($params['publicationDate'])) {
@@ -40,10 +40,10 @@ class Article
 			$pdo->exec("SET NAMES 'utf8'");
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql = "SELECT *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles WHERE id = :id";
-			$st = $pdo->prepare( $sql );
-			$st->bindValue( ":id", $id, PDO::PARAM_INT );
-			$st->execute();
-			$row = $st->fetch();
+			$s = $pdo->prepare( $sql );
+			$s->bindValue( ":id", $id, PDO::PARAM_INT );
+			$s->execute();
+			$row = $s->fetch();
 			$pdo = null;
 		} catch (PDOException $e) {
 			echo 'Ошибка в Article::getById($id)';
@@ -60,12 +60,12 @@ class Article
 			$sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles
 							ORDER BY " . $pdo->quote($order) . " LIMIT :numRows";
 
-			$st = $pdo->prepare($sql);
-			$st->bindValue(":numRows", $numRows, PDO::PARAM_INT);
-			$st->execute();
+			$s = $pdo->prepare($sql);
+			$s->bindValue(":numRows", $numRows, PDO::PARAM_INT);
+			$s->execute();
 			$list = array();
 
-			while ($row = $st->fetch()) {
+			while ($row = $s->fetch()) {
 				$article = new Article($row);
 				$list[] = $article;
 			}
@@ -88,12 +88,12 @@ class Article
 			$pdo->exec("SET NAMES 'utf8'");
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql = "INSERT INTO articles (publicationDate, title, summary, content) VALUES (FROM_UNIXTIME(:publicationDate), :title, :summary, :content)";
-			$st = $pdo->prepare ($sql);
-			$st->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
-			$st->bindValue(":title", $this->title, PDO::PARAM_STR);
-			$st->bindValue(":summary", $this->summary, PDO::PARAM_STR);
-			$st->bindValue(":content", $this->content, PDO::PARAM_STR);
-			$st->execute();
+			$s = $pdo->prepare ($sql);
+			$s->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
+			$s->bindValue(":title", $this->title, PDO::PARAM_STR);
+			$s->bindValue(":summary", $this->summary, PDO::PARAM_STR);
+			$s->bindValue(":content", $this->content, PDO::PARAM_STR);
+			$s->execute();
 			$this->id = $pdo->lastInsertId();
 			$pdo = null;
 		} catch (PDOException $e) {
@@ -111,13 +111,13 @@ class Article
 			$pdo->exec("SET NAMES 'utf8'");
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql = "UPDATE articles SET publicationDate=FROM_UNIXTIME(:publicationDate), title=:title, summary=:summary, content=:content WHERE id = :id";
-			$st = $pdo->prepare ($sql);
-			$st->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
-			$st->bindValue(":title", $this->title, PDO::PARAM_STR);
-			$st->bindValue(":summary", $this->summary, PDO::PARAM_STR);
-			$st->bindValue(":content", $this->content, PDO::PARAM_STR);
-			$st->bindValue(":id", $this->id, PDO::PARAM_INT);
-			$st->execute();
+			$s = $pdo->prepare ($sql);
+			$s->bindValue(":publicationDate", $this->publicationDate, PDO::PARAM_INT);
+			$s->bindValue(":title", $this->title, PDO::PARAM_STR);
+			$s->bindValue(":summary", $this->summary, PDO::PARAM_STR);
+			$s->bindValue(":content", $this->content, PDO::PARAM_STR);
+			$s->bindValue(":id", $this->id, PDO::PARAM_INT);
+			$s->execute();
 			$pdo = null;
 		} catch (PDOException $e) {
 			echo 'Ошибка в Article::update()';
@@ -133,9 +133,9 @@ class Article
 			$pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
 			$pdo->exec("SET NAMES 'utf8'");
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$st = $pdo->prepare ( "DELETE FROM articles WHERE id = :id LIMIT 1" );
-			$st->bindValue( ":id", $this->id, PDO::PARAM_INT );
-			$st->execute();
+			$s = $pdo->prepare ( "DELETE FROM articles WHERE id = :id LIMIT 1" );
+			$s->bindValue( ":id", $this->id, PDO::PARAM_INT );
+			$s->execute();
 			$pdo = null;
 		} catch (PDOException $e) {
 			echo 'Ошибка в Article::delete()';
