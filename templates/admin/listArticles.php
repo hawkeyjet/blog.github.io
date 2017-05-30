62d2216990dd43768171723946b284fff6ca277b
Andy <hawkeyjet@gmail.com>
2017-05-30 09:52:44 +0300

    ok

--- templates/admin/listArticles.php
+++ templates/admin/listArticles.php
@@ -1,43 +1,41 @@
 <?php include "templates/include/header.php" ?>
 
-      <div id="adminHeader">
-        <h2>Widget News Admin</h2>
-        <p>You are logged in as <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="admin.php?action=logout"?>Log out</a></p>
-      </div>
+			<div id="adminHeader">
+				<h2>Блог (Администратор)</h2>
+				<p>Вы вошли как <b><?php echo htmlout($_SESSION['username'])?></b>. <a href="admin.php?action=logout"?>Выйти</a></p>
+			</div>
 
-      <h1>All Articles</h1>
+			<h1>Все статьи</h1>
 
-<?php if ( isset( $results['errorMessage'] ) ) { ?>
-        <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
+<?php if (isset( $results['errorMessage'])) { ?>
+				<div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
 <?php } ?>
 
 
-<?php if ( isset( $results['statusMessage'] ) ) { ?>
-        <div class="statusMessage"><?php echo $results['statusMessage'] ?></div>
+<?php if (isset($results['statusMessage'])) { ?>
+				<div class="statusMessage"><?php echo $results['statusMessage'] ?></div>
 <?php } ?>
 
-      <table>
-        <tr>
-          <th>Publication Date</th>
-          <th>Article</th>
-        </tr>
+			<table>
+				<tr>
+					<th>Дата публикации</th>
+					<th>Статья</th>
+				</tr>
 
-<?php foreach ( $results['articles'] as $article ) { ?>
-
-        <tr onclick="location='admin.php?action=editArticle&amp;articleId=<?php echo $article->id?>'">
-          <td><?php echo date('j M Y', $article->publicationDate)?></td>
-          <td>
-            <?php echo $article->title?>
-          </td>
-        </tr>
-
+<?php foreach ($results['articles'] as $article) { ?>
+				<tr onclick="location='admin.php?action=editArticle&amp;articleId=<?php echo $article->id?>'">
+					<td><?php echo date('j M Y', $article->publicationDate)?></td>
+					<td>
+						<?php echo $article->title?>
+					</td>
+				</tr>
 <?php } ?>
 
-      </table>
+			</table>
 
-      <p><?php echo $results['totalRows']?> article<?php echo ( $results['totalRows'] != 1 ) ? 's' : '' ?> in total.</p>
+			<p>Количество статей: <?php echo $results['totalRows']?></p>
 
-      <p><a href="admin.php?action=newArticle">Add a New Article</a></p>
+			<p><a href="admin.php?action=newArticle">Добавить новую статью</a></p>
 
 <?php include "templates/include/footer.php" ?>
 
--- templates/admin/loginForm.php
+++ templates/admin/loginForm.php
@@ -1,31 +1,30 @@
 <?php include "templates/include/header.php" ?>
 
-      <form action="admin.php?action=login" method="post" style="width: 50%;">
-        <input type="hidden" name="login" value="true" />
+			<form action="admin.php?action=login" method="post" style="width: 50%;">
+				<input type="hidden" name="login" value="true" />
 
-<?php if ( isset( $results['errorMessage'] ) ) { ?>
-        <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
+<?php if (isset($results['errorMessage'])) { ?>
+				<div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
 <?php } ?>
 
-        <ul>
+				<ul>
 
-          <li>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Your admin username" required autofocus maxlength="20" />
-          </li>
+					<li>
+						<label for="username">Логин</label>
+						<input type="text" name="username" id="username" placeholder="Логин Администратора" required autofocus maxlength="20" />
+					</li>
 
-          <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your admin password" required maxlength="20" />
-          </li>
+					<li>
+						<label for="password">Пароль</label>
+						<input type="password" name="password" id="password" placeholder="Пароль Администратора" required maxlength="20" />
+					</li>
 
-        </ul>
+				</ul>
 
-        <div class="buttons">
-          <input type="submit" name="login" value="Login" />
-        </div>
+				<div class="buttons">
+					<input type="submit" name="login" value="Войти" />
+				</div>
 
-      </form>
+			</form>
 
 <?php include "templates/include/footer.php" ?>
-
