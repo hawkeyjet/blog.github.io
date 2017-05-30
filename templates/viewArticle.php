56933e1fb136b6c88face3e8376da328f6e72c4a
Andy <hawkeyjet@gmail.com>
2017-05-30 08:41:51 +0300

    viewArticle

--- templates/viewArticle.php
+++ templates/viewArticle.php
@@ -1,11 +1,14 @@
-<?php include "templates/include/header.php" ?>
+<?php
+	include "templates/include/header.php";
+	include "templates/include/htmlout.inc.php";
+?>
 
-      <h1 style="width: 75%;"><?php echo htmlspecialchars( $results['article']->title )?></h1>
-      <div style="width: 75%; font-style: italic;"><?php echo htmlspecialchars( $results['article']->summary )?></div>
-      <div style="width: 75%;"><?php echo $results['article']->content?></div>
-      <p class="pubDate">Published on <?php echo date('j F Y', $results['article']->publicationDate)?></p>
+			<h1 style="width: 75%;"><?php echo htmlout($results['article']->title)?></h1>
+			<div style="width: 75%; font-style: italic;"><?php echo htmlout($results['article']->summary)?></div>
+			<div style="width: 75%;"><?php echo $results['article']->content ?></div>
+			<p class="pubDate">Опубликовано <?php echo date('j F Y', $results['article']->publicationDate)?></p>
 
-      <p><a href="./">Return to Homepage</a></p>
+			<p><a href="./">Вернутся на Главную</a></p>
 
 <?php include "templates/include/footer.php" ?>
 
